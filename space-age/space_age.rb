9d4023359ca0a6c6f6ffb4713b677deb487b8353
Michael Nash <michael@spatialnetworks.com>
2018-01-06 21:31:56 -0500

    dynamically generate methods

--- space-age/space_age.rb
+++ space-age/space_age.rb
@@ -1,4 +1,14 @@
 class SpaceAge
+  ORBITAL_PERIODS = {
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
+
   def initialize(time_in_seconds)
     @time_in_seconds = time_in_seconds
   end
@@ -7,34 +17,11 @@ class SpaceAge
     @time_in_seconds.to_f / 31_557_600.to_f
   end
 
-  def on_mercury
-    on_earth / 0.2408467
+  ORBITAL_PERIODS.each do |planet, orbital_period|
+    define_method "on_#{planet}" do
+      on_earth / orbital_period
+    end
   end
-
-  def on_venus
-    on_earth / 0.61519726
-  end
-
-  def on_mars
-    on_earth / 1.8808158
-  end
-
-  def on_jupiter
-    on_earth / 11.862615
-  end
-
-  def on_saturn
-    on_earth / 29.447498
-  end
-
-  def on_uranus
-    on_earth / 84.016846
-  end
-
-  def on_neptune
-    on_earth / 164.79132
-  end
-
 end
 
 class BookKeeping
