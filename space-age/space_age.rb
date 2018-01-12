be0e2cfc07424198273251f3947aae5ea018b7b5
Michael Nash <michael@spatialnetworks.com>
2018-01-12 11:44:26 -0500

    generalized the 'on_earth' case

--- space-age/space_age.rb
+++ space-age/space_age.rb
@@ -1,5 +1,6 @@
 class SpaceAge
   ORBITAL_PERIODS = {
+    earth: 1.00,
     mercury: 0.2408467,
     venus: 0.61519726,
     mars: 1.8808158,
@@ -9,17 +10,15 @@ class SpaceAge
     neptune: 164.79132
   }
 
+  EARTH_YEAR_SECONDS = 31_557_600.to_f
+
   def initialize(time_in_seconds)
     @time_in_seconds = time_in_seconds
   end
 
-  def on_earth
-    @time_in_seconds.to_f / 31_557_600.to_f
-  end
-
   ORBITAL_PERIODS.each do |planet, orbital_period|
     define_method "on_#{planet}" do
-      on_earth / orbital_period
+      (@time_in_seconds.to_f / EARTH_YEAR_SECONDS) / orbital_period
     end
   end
 end
