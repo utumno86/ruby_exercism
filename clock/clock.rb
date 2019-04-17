c767fb5722433889b78894e2a1bd9018469863b0
Michael Nash <michael@spatialnetworks.com>
2019-04-17 13:33:38 -0400

    re-write basic clock functionality

--- clock/clock.rb
+++ clock/clock.rb
@@ -1,11 +1,13 @@
 class Clock
   def initialize(initial_clock_values = { hour: 0, minute: 0 })
-    @hours = initial_clock_values[:hour] || 0
-    @minutes = process_minutes(initial_clock_values[:minute] || 0)
+    @total_minutes = total_minutes(initial_clock_values[:hour], initial_clock_values[:minute])
+    @hours = 0
   end
 
   def to_s
-    "#{calculate_hours(@hours)}:#{calculate_minutes(@minutes)}"
+    calculate_hours
+    calculate_minutes
+    "#{print_hours}:#{print_minutes}"
   end
 
   def +(other)
@@ -28,47 +30,50 @@ class Clock
 
   private
 
-  def calculate_hours(hours)
-    case hours
-    when 0..9
-      "0#{hours}"
-    when 10..23
-      hours.to_s
-    else
-      if hours > 0
-        calculate_hours(hours - 24)
-      else
-        calculate_hours(24 + hours)
-      end
-    end
+  def total_minutes(hours, minutes)
+    (hours.to_i * 60) + minutes.to_i
   end
 
-  def calculate_minutes(minutes)
-    if minutes > 0
-      minutes < 10 ? "0#{minutes}" : minutes
-    else
-      '00'
-    end
+  def calculate_hours
+    @total_minutes < 0 ? negative_hours : positive_hours
   end
 
-  def process_minutes(minutes, subtract=false)
-    total_minutes = process_total_minutes(minutes, subtract)
-    while total_minutes >= 60
-      @hours += 1
-      total_minutes -= 60
-    end
-    while total_minutes < 0
+  def negative_hours
+    while @total_minutes < -59
+      @total_minutes += 60
       @hours -= 1
-      total_minutes += 60
+      normalize_hours
     end
-    total_minutes
   end
 
-  def process_total_minutes(minutes, subtract)
-    if subtract
-      @minutes ? (@minutes - minutes).to_i : minutes.to_i
-    else
-      @minutes ? (@minutes + minutes).to_i : minutes.to_i
+  def positive_hours
+    while @total_minutes > 59
+      @total_minutes -= 60
+      @hours += 1
+      normalize_hours
     end
+  end
+
+  def calculate_minutes
+    if @total_minutes < 0
+      @hours -= 1
+      @total_minutes = 60 + @total_minutes
+    else
+      @total_minutes
+    end
+  end
+
+  def print_hours
+    normalize_hours
+    @hours > 9 ? @hours.to_s : "0#{@hours}"
+  end
+
+  def print_minutes
+    @total_minutes > 9 ? @total_minutes.to_s : "0#{@total_minutes}"
+  end
+
+  def normalize_hours
+    @hours -= 24 if @hours >= 24
+    @hours += 24 if @hours < 0
   end
 end
