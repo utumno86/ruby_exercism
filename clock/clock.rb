cfee336bf9feb57f6e0d0d2894b54fbebc736234
Michael Nash <michael@spatialnetworks.com>
2019-04-22 14:14:00 -0400

    refactor clock

--- clock/clock.rb
+++ clock/clock.rb
@@ -1,77 +1,27 @@
 class Clock
+  attr_reader :minutes
+
   def initialize(hour: 0, minute: 0)
-    @total_minutes = total_minutes(hour, minute)
-    @hours = 0
+    @minutes = (hour * 60) + minute
   end
 
   def to_s
-    calculate_hours
-    calculate_minutes
-    "#{print_hours}:#{print_minutes}"
+    hours = (@minutes / 60) % 24
+    minutes = @minutes % 60
+    format('%02d:%02d', hours, minutes)
   end
 
   def +(other)
-    hours, minutes = other.to_s.split(':')
-    @total_minutes += total_minutes(hours, minutes)
+    @minutes += other.minutes
     to_s
   end
 
   def -(other)
-    hours, minutes = other.to_s.split(':')
-    @total_minutes -= total_minutes(hours, minutes)
+    @minutes -= other.minutes
     to_s
   end
 
   def ==(other)
-    other.to_s == to_s
-  end
-
-  private
-
-  def total_minutes(hours, minutes)
-    (hours.to_i * 60) + minutes.to_i
-  end
-
-  def calculate_hours
-    @total_minutes < 0 ? negative_hours : positive_hours
-  end
-
-  def negative_hours
-    while @total_minutes < -59
-      @total_minutes += 60
-      @hours -= 1
-      normalize_hours
-    end
-  end
-
-  def positive_hours
-    while @total_minutes > 59
-      @total_minutes -= 60
-      @hours += 1
-      normalize_hours
-    end
-  end
-
-  def calculate_minutes
-    if @total_minutes < 0
-      @hours -= 1
-      @total_minutes = 60 + @total_minutes
-    else
-      @total_minutes
-    end
-  end
-
-  def print_hours
-    normalize_hours
-    @hours > 9 ? @hours.to_s : "0#{@hours}"
-  end
-
-  def print_minutes
-    @total_minutes > 9 ? @total_minutes.to_s : "0#{@total_minutes}"
-  end
-
-  def normalize_hours
-    @hours -= 24 if @hours >= 24
-    @hours += 24 if @hours < 0
+    to_s == other.to_s
   end
 end
