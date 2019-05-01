9acd99f72adee8ef3d25e47f8877da2316327e4b
Michael Nash <michael@spatialnetworks.com>
2019-05-01 11:51:00 -0400

    completed high-scores

---
+++ high-scores/high_scores.rb
@@ -0,0 +1,20 @@
+# Class to return specific High Scores for Frogger
+class HighScores
+  attr_reader :scores
+
+  def initialize(scores)
+    @scores = scores
+  end
+
+  def latest
+    @scores[-1]
+  end
+
+  def personal_best
+    @scores.max
+  end
+
+  def personal_top_three
+    @scores.sort.reverse[0..2]
+  end
+end
