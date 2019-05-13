c431b204c60c2adf087c47b462175bbb4251121e
Michael Nash <michael@spatialnetworks.com>
2019-05-13 12:42:58 -0400

    iterated matrix

--- matrix/matrix.rb
+++ matrix/matrix.rb
@@ -3,23 +3,7 @@ class Matrix
   attr_reader :columns
 
   def initialize(matrix_string)
-    @rows = matrix_rows(matrix_string).map { |line| line.split(' ').map(&:to_i) }
-    @columns = column_calc(matrix_rows(matrix_string))
+    @rows = matrix_string.lines.map { |line| line.split(' ').map(&:to_i) }
+    @columns = @rows.transpose
   end
-
-  def matrix_rows(matrix_string)
-    matrix_string.split("\n")
-  end
-
-  def column_calc(lines)
-    columns = []
-    lines.each do |line|
-      line.split(' ').each_with_index do |num, index|
-        columns[index] ||= []
-        columns[index] << num.to_i
-      end
-    end
-    columns
-  end
-
 end
