484342f9f20579be1f6f4717a64130b84942767e
Michael Nash <michael@spatialnetworks.com>
2019-05-13 16:08:01 -0400

    isogram solution

---
+++ isogram/isogram.rb
@@ -0,0 +1,10 @@
+# Class for testing whether or not a string is an isogram
+class Isogram
+  def self.isogram?(test_string)
+    normalize(test_string).split('').uniq.length == normalize(test_string).length
+  end
+
+  def self.normalize(test_string)
+    test_string.gsub(/\W/, '').downcase
+  end
+end
