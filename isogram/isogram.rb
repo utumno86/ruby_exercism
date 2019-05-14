1790ddec48d4c6fbb9760ca4152551369eece9ce
Michael Nash <michael@spatialnetworks.com>
2019-05-14 13:41:46 -0400

    completed isogram

--- isogram/isogram.rb
+++ isogram/isogram.rb
@@ -1,7 +1,7 @@
 # Class for testing whether or not a string is an isogram
 class Isogram
   def self.isogram?(test_string)
-    normalize(test_string).split('').uniq.length == normalize(test_string).length
+    normalize(test_string).chars.uniq.length == normalize(test_string).length
   end
 
   def self.normalize(test_string)
