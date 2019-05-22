0ed6b97976cfb900359ef097e5c4029632f68edf
Michael Nash <michael@spatialnetworks.com>
2019-05-22 11:52:14 -0400

    luhn second update

--- luhn/luhn.rb
+++ luhn/luhn.rb
@@ -1,45 +1,43 @@
+# frozen_string_literal: true
+
 # Class for determining the validity of credit card numbers
 class Luhn
   def self.valid?(number_string)
     Luhn.new(number_string).valid?
   end
 
-  def initialize(number_string)
-    @number_string = number_string
+  def initialize(number)
+    @number = number
   end
 
   def valid?
-    return false unless clean?
+    return false unless long_enough? && only_numbers_and_spaces?
 
     (checksum % 10).zero?
   end
 
   def checksum
-    number_array.reverse.each_slice(2).sum { |first, second=0| first + double_less_than_nine(second) }
-  end
-
-  def clean?
-    long_enough? && only_numbers_and_spaces?
+    digits.reverse.each_slice(2).sum { |first, second=0| first + double(second) }
   end
 
   def long_enough?
-    stripped_number_string.length > 1
+    stripped_number.length > 1
   end
 
-  def double_less_than_nine(number)
-    doubled = number * 2
+  def double(digit)
+    doubled = digit * 2
     doubled > 9 ? doubled - 9 : doubled
   end
 
   def only_numbers_and_spaces?
-    !@number_string.match(/[^0-9 ]/)
+    !@number.match(/[^0-9 ]/)
   end
 
-  def stripped_number_string
-    @stripped_number_string || @number_string.gsub(/\D/, '')
+  def stripped_number
+    @stripped_number ||= @number.gsub(/\D/, '')
   end
 
-  def number_array
-    @number_array || stripped_number_string.each_char.map(&:to_i)
+  def digits
+    @digits ||= stripped_number_string.each_char.map(&:to_i)
   end
 end
