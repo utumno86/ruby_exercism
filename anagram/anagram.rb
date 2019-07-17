30d92b8dda31dedf7267e27fa78bc36a6538e8a9
Michael Nash <michael@spatialnetworks.com>
2019-07-17 11:54:22 -0400

    updated anagram

--- anagram/anagram.rb
+++ anagram/anagram.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# class to return whether or not a word is an anagram
 class Anagram
   def initialize(word)
     @word = word
@@ -6,8 +9,4 @@ class Anagram
   def match(candidates)
     candidates.find_all{ |candidate| candidate.downcase.chars.sort.join == @word.downcase.chars.sort.join && candidate.downcase != @word.downcase }
   end
-end
-
-class BookKeeping
-  VERSION = 2
 end
