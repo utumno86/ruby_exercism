686d5dc21db031245801d281736e8873167c8304
Michael Nash <michael.nash@kin.com>
2026-06-04 19:20:46 -0400

    savings account

--- savings-account/savings_account.rb
+++ savings-account/savings_account.rb
@@ -1,13 +1,27 @@
 module SavingsAccount
   def self.interest_rate(balance)
-    raise 'Please implement the SavingsAccount.interest_rate method'
+    case balance
+    when 0..999.999
+      0.5
+    when 1000..4999.999
+      1.621
+    when 5000..Float::INFINITY
+      2.475
+    else
+      3.213
+    end
   end
 
   def self.annual_balance_update(balance)
-    raise 'Please implement the SavingsAccount.annual_balance_update method'
+    balance + (balance * interest_rate(balance) / 100)
   end
 
   def self.years_before_desired_balance(current_balance, desired_balance)
-    raise 'Please implement the SavingsAccount.years_before_desired_balance method'
+    years = 0
+    while current_balance < desired_balance
+      current_balance = annual_balance_update(current_balance)
+      years += 1
+    end
+    years
   end
 end
