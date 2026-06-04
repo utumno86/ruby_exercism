1a04f80506d95ff99c47a4f7cbb95bf6f80338cd
Michael Nash <michael.nash@kin.com>
2026-06-04 19:01:26 -0400

    assembly line

--- assembly-line/assembly_line.rb
+++ assembly-line/assembly_line.rb
@@ -4,10 +4,21 @@ class AssemblyLine
   end
 
   def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    case @speed
+    when 0..4
+      @speed * 221
+    when 5..8
+      @speed * 221 * 0.9
+    when 9
+      @speed * 221 * 0.8
+    when 10
+      @speed * 221 * 0.77
+    else
+      raise 'Invalid speed'
+    end
   end
 
   def working_items_per_minute
-    raise 'Please implement the AssemblyLine#working_items_per_minute method'
+    (production_rate_per_hour / 60).floor
   end
 end
